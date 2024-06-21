13eb64b8a5c151476a993dfd78fb200cdd8f1f9e
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-06-21 23:59:12 +0100

    Dragable ships UI first steps

--- src/index.js
+++ src/index.js
@@ -6,20 +6,40 @@ import './ui.js';
 const Gameboard = require('./classes/gameboard.js');
 const UI = require('./ui.js');
 
-let gameboardOne = {
-  'data': Gameboard(),
-  'ui': UI.createGameboardUI(true)
-}
-
-let gameboardTwo = {
-  'data': Gameboard(),
-  'ui': UI.createGameboardUI(false)
-}
-
 const main = document.getElementsByTagName('main')[0];
-main.appendChild(gameboardOne.ui);
-main.appendChild(gameboardTwo.ui);
+const planningGrid = document.getElementsByClassName('planning-grid')[0];
 
-gameboardOne.ui.addEventListener('cellClicked', (event) => {
-  console.log(event.detail);
-});
+function createSelectionUI() {
+  let shipSelectionBoard = {
+    'data': Gameboard(),
+    'ui': UI.createGameboardUI(true)
+  }
+
+  shipSelectionBoard.ui.style.width = '70%';
+  shipSelectionBoard.ui.style.height = '70%';
+  shipSelectionBoard.ui.style.aspectRatio = '1';
+
+  planningGrid.prepend(shipSelectionBoard.ui);
+}
+
+createSelectionUI();
+// let gameboardOne = {
+//   'data': Gameboard(),
+//   'ui': UI.createGameboardUI(true)
+// }
+
+// let gameboardTwo = {
+//   'data': Gameboard(),
+//   'ui': UI.createGameboardUI(false)
+// }
+
+// main.appendChild(gameboardOne.ui);
+// main.appendChild(gameboardTwo.ui);
+
+// gameboardOne.ui.addEventListener('cellClicked', (event) => {
+//   console.log(event.detail);
+// });
+
+// gameboardTwo.ui.addEventListener('cellClicked', (event) => {
+//   console.log(event.detail);
+// });
--- src/ui.js
+++ src/ui.js
@@ -49,7 +49,7 @@ function createGameboardUI(isFriendly) {
     }
   }
 
-  return containerFrame
+  return containerFrame;
 }
 
 module.exports = { createGameboardUI };
