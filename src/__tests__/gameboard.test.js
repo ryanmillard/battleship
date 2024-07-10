3ede1306df93582e1a5b2fc2dd7b62f3ed3ba58c
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-07-10 23:41:00 +0100

    Resize gameboard and reposition based on resolution

--- src/__tests__/gameboard.test.js
+++
@@ -1,7 +0,0 @@
-const gameboard = require('../classes/gameboard.js');
-
-describe('Gameboard Class', () => {
-  it('generates board', () => {
-    let gb = gameboard();
-  });
-});
--- src/__tests__/ship.test.js
+++
@@ -1,9 +0,0 @@
-const Ship = require('../classes/ship.js');
-
-describe('Ship Class', () => {
-  it('generates attributes', () => {
-    let x = Ship();
-    expect(x.length).toBe(0);
-    expect(x.timesHit).toBe(0);
-  });
-});
